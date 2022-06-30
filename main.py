4f8b4e4eb4f097eb8efedb7d1ef3fd0ffb83e15d
emoronayuso <emoronayuso@gmail.com>
2022-06-30 02:18:31 +0200

    feat: Add cloudfunction

---
+++ main.py
@@ -0,0 +1,38 @@
+import os
+from google.cloud import pubsub_v1
+
+#credentials_path = '/PATH/TO/YOU/PRIVATE/JSON/FILE/HERE/myFile.privateKey.json'
+#os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credentials_path
+
+def publisher_cf(event, context):
+    """Background Cloud Function to be triggered by Cloud Storage.
+       This generic function logs relevant data when a file is changed,
+       and works for all Cloud Storage CRUD operations.
+    Args:
+        event (dict):  The dictionary with data specific to this type of event.
+                       The `data` field contains a description of the event in
+                       the Cloud Storage `object` format described here:
+                       https://cloud.google.com/storage/docs/json_api/v1/objects#resource
+        context (google.cloud.functions.Context): Metadata of triggering event.
+    Returns:
+        None; the output is written to Cloud Logging
+    """
+
+    print('Event ID: {}'.format(context.event_id))
+    print('Event type: {}'.format(context.event_type))
+    print('Bucket: {}'.format(event['bucket']))
+    print('File: {}'.format(event['name']))
+    print('Metageneration: {}'.format(event['metageneration']))
+    print('Created: {}'.format(event['timeCreated']))
+    print('Updated: {}'.format(event['updated']))
+
+    publisher = pubsub_v1.PublisherClient()
+    topic_path = 'projects/pruebas-pubsub-systerminal/topics/topic-cf'
+
+    data = 'A garden sensor is ready!'
+    data = data.encode('utf-8')
+    attributes = {
+       'sensorName': 'garden-001',
+       'temperature': '75.0',
+       'humidity': '60'
+    }
